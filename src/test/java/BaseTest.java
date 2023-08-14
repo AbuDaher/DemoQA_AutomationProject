aa6781975db5a5dd1886ae778474a501483b5f9d
andreea.abu-daher <andreea.abudaher@gmail.com>
2023-08-14 14:51:34 +0300

    Optimizare proiect

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,36 +1,28 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import constants.Constants;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.Books;
+import pages.HomePage;
 
 public class BaseTest {
-    @Test
-    public void goToWebsite(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
+    protected WebDriver driver;
+    protected HomePage homePage;
+    protected Books books;
+    @BeforeMethod
+    public void setup(){
+        this.driver = new ChromeDriver();
+        this.driver.get(Constants.URL);
+        this.driver.manage().window().maximize();
+        this.homePage = new HomePage(driver);
+        this.books = new Books(driver);
     }
 
     @Test
-    public void logInTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
-        Thread.sleep(1000);
-        WebElement element = driver.findElement(By.xpath("(//div[@class='card-up'])[6]"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        driver.findElement(By.xpath("(//div[@class='card-up'])[6]")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.cssSelector("#login")).click();
-        driver.findElement(By.cssSelector("#userName")).sendKeys("Andreea");
-        driver.findElement(By.cssSelector("#password")).sendKeys("Andreea123!");
-        driver.findElement(By.cssSelector("#login")).click();
-        Thread.sleep(1000);
-        String expectedMessage = "Andreea";
-        String actualMessage = driver.findElement(By.cssSelector("#userName-value")).getText();
-        Assert.assertEquals(actualMessage, expectedMessage, "Error: User name is not present");
 
-        driver.close();
+    public void goToWebSite(){
+
     }
+
 }
