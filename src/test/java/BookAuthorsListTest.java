2b6109bade0d45dd930ee78be8d29ec14bd176a5
andreea.abu-daher <andreea.abudaher@gmail.com>
2023-09-01 09:52:11 +0300

    Homework Finished

--- src/test/java/BookAuthorsListTest.java
+++ src/test/java/BookAuthorsListTest.java
@@ -1,11 +1,12 @@
 import org.testng.annotations.Test;
 import utils.Utils;
 
-public class BookAuthorsListTest extends BaseTest{
+public class BookAuthorsListTest extends BaseTest {
 
     @Test
-    public void getAllBooksAuthors (){
+    public void getAllBooksAuthors() {
         this.homePage.clickBookStoreApplicationCard();
         Utils.waitInSeconds(1);
         books.getAllBooksAuthors();
     }
+}
